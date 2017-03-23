427b24b363bda0fd178ef3b787c1e3bc68ce668a
znzp124900171 <znzp124900171@gmail.com>
2017-03-23 20:24:34 +0100

    添加了左边栏点击样式

--- WebGL GUI/dist/js/app.js
+++ WebGL GUI/dist/js/app.js
@@ -53,11 +53,7 @@ $.GUI.options = {
     //This option is forced to true if both the fixed layout and sidebar mini
     //are used together
     sidebarExpandOnHover: false,
-    //BoxRefresh Plugin
-    enableBoxRefresh: true,
-    //Bootstrap.js tooltip
-    enableBSToppltip: true,
-    BSTooltipSelector: "[data-toggle='tooltip']",
+
     //Enable Fast Click. Fastclick.js creates a more
     //native touch experience with touch devices. If you
     //choose to enable the plugin, make sure you load the script
@@ -75,33 +71,7 @@ $.GUI.options = {
         //Enable slide over content
         slide: true
     },
-    //Box Widget Plugin. Enable this plugin
-    //to allow boxes to be collapsed and/or removed
-    enableBoxWidget: true,
-    //Box Widget plugin options
-    boxWidgetOptions: {
-        boxWidgetIcons: {
-            //Collapse icon
-            collapse: 'fa-minus',
-            //Open icon
-            open: 'fa-plus',
-            //Remove icon
-            remove: 'fa-times'
-        },
-        boxWidgetSelectors: {
-            //Remove button selector
-            remove: '[data-widget="remove"]',
-            //Collapse button selector
-            collapse: '[data-widget="collapse"]'
-        }
-    },
-    //Direct Chat plugin options
-    directChat: {
-        //Enable direct chat by default
-        enable: true,
-        //The button to open and close the chat contacts pane
-        contactToggleSelector: '[data-widget="chat-pane-toggle"]'
-    },
+
     //Define the set of colors to use globally around the website
     colors: {
         lightBlue: "#3c8dbc",
@@ -185,30 +155,9 @@ $(function() {
         $.GUI.pushMenu.activate(o.sidebarToggleSelector);
     }
 
-    //Activate Bootstrap tooltip
-    if (o.enableBSToppltip) {
-        $('body').tooltip({
-            selector: o.BSTooltipSelector,
-            container: 'body'
-        });
-    }
-
-    //Activate box widget
-    if (o.enableBoxWidget) {
-        $.GUI.boxWidget.activate();
-    }
-
     //Activate fast click
     if (o.enableFastclick && typeof FastClick != 'undefined') {
         FastClick.attach(document.body);
-    }
-
-    //Activate direct chat widget
-    if (o.directChat.enable) {
-        $(document).on('click', o.directChat.contactToggleSelector, function() {
-            var box = $(this).parents('.direct-chat').first();
-            box.toggleClass('direct-chat-contacts-open');
-        });
     }
 
     /*
@@ -440,8 +389,21 @@ function _init() {
                 }
             });
 
-        $('.treeview .treeview-menu').on('click', 'a', function() {
-            $('.active').removeClass('active');
+        $('#model .treeview-menu').on('click', 'a', function(e) {
+            e.preventDefault;
+            $('#model .active').removeClass('active');
+            $(this).addClass('active');
+        })
+
+        $('#plotGroup .treeview-menu').on('click','a',function(e){
+            e.preventDefault;
+            $('#plotGroup .active').removeClass('active');
+            $(this).addClass('active');
+        })
+
+        $('#plot .treeview-menu').on('click','a',function(e){
+            e.preventDefault;
+            $('#plot .active').removeClass('active');
             $(this).addClass('active');
         })
     };
@@ -542,239 +504,8 @@ function _init() {
             $(".content-wrapper, .right-side").css('min-height', sidebar.height());
         }
     };
-
-    /* BoxWidget
-     * =========
-     * BoxWidget is a plugin to handle collapsing and
-     * removing boxes from the screen.
-     *
-     * @type Object
-     * @usage $.GUI.boxWidget.activate()
-     *        Set all your options in the main $.GUI.options object
-     */
-    $.GUI.boxWidget = {
-        selectors: $.GUI.options.boxWidgetOptions.boxWidgetSelectors,
-        icons: $.GUI.options.boxWidgetOptions.boxWidgetIcons,
-        animationSpeed: $.GUI.options.animationSpeed,
-        activate: function(_box) {
-            var _this = this;
-            if (!_box) {
-                _box = document; // activate all boxes per default
-            }
-            //Listen for collapse event triggers
-            $(_box).on('click', _this.selectors.collapse, function(e) {
-                e.preventDefault();
-                _this.collapse($(this));
-            });
-
-            //Listen for remove event triggers
-            $(_box).on('click', _this.selectors.remove, function(e) {
-                e.preventDefault();
-                _this.remove($(this));
-            });
-        },
-        collapse: function(element) {
-            var _this = this;
-            //Find the box parent
-            var box = element.parents(".box").first();
-            //Find the body and the footer
-            var box_content = box.find("> .box-body, > .box-footer, > form  >.box-body, > form > .box-footer");
-            if (!box.hasClass("collapsed-box")) {
-                //Convert minus into plus
-                element.children(":first")
-                    .removeClass(_this.icons.collapse)
-                    .addClass(_this.icons.open);
-                //Hide the content
-                box_content.slideUp(_this.animationSpeed, function() {
-                    box.addClass("collapsed-box");
-                });
-            } else {
-                //Convert plus into minus
-                element.children(":first")
-                    .removeClass(_this.icons.open)
-                    .addClass(_this.icons.collapse);
-                //Show the content
-                box_content.slideDown(_this.animationSpeed, function() {
-                    box.removeClass("collapsed-box");
-                });
-            }
-        },
-        remove: function(element) {
-            //Find the box parent
-            var box = element.parents(".box").first();
-            box.slideUp(this.animationSpeed);
-        }
-    };
 }
 
-/* ------------------
- * - Custom Plugins -
- * ------------------
- * All custom plugins are defined below.
- */
-
-/*
- * BOX REFRESH BUTTON
- * ------------------
- * This is a custom plugin to use with the component BOX. It allows you to add
- * a refresh button to the box. It converts the box's state to a loading state.
- *
- * @type plugin
- * @usage $("#box-widget").boxRefresh( options );
- */
-(function($) {
-
-    "use strict";
-
-    $.fn.boxRefresh = function(options) {
-
-        // Render options
-        var settings = $.extend({
-            //Refresh button selector
-            trigger: ".refresh-btn",
-            //File source to be loaded (e.g: ajax/src.php)
-            source: "",
-            //Callbacks
-            onLoadStart: function(box) {
-                return box;
-            }, //Right after the button has been clicked
-            onLoadDone: function(box) {
-                    return box;
-                } //When the source has been loaded
-
-        }, options);
-
-        //The overlay
-        var overlay = $('<div class="overlay"><div class="fa fa-refresh fa-spin"></div></div>');
-
-        return this.each(function() {
-            //if a source is specified
-            if (settings.source === "") {
-                if (window.console) {
-                    window.console.log("Please specify a source first - boxRefresh()");
-                }
-                return;
-            }
-            //the box
-            var box = $(this);
-            //the button
-            var rBtn = box.find(settings.trigger).first();
-
-            //On trigger click
-            rBtn.on('click', function(e) {
-                e.preventDefault();
-                //Add loading overlay
-                start(box);
-
-                //Perform ajax call
-                box.find(".box-body").load(settings.source, function() {
-                    done(box);
-                });
-            });
-        });
-
-        function start(box) {
-            //Add overlay and loading img
-            box.append(overlay);
-
-            settings.onLoadStart.call(box);
-        }
-
-        function done(box) {
-            //Remove overlay and loading img
-            box.find(overlay).remove();
-
-            settings.onLoadDone.call(box);
-        }
-
-    };
-
-})(jQuery);
-
-/*
- * EXPLICIT BOX CONTROLS
- * -----------------------
- * This is a custom plugin to use with the component BOX. It allows you to activate
- * a box inserted in the DOM after the app.js was loaded, toggle and remove box.
- *
- * @type plugin
- * @usage $("#box-widget").activateBox();
- * @usage $("#box-widget").toggleBox();
- * @usage $("#box-widget").removeBox();
- */
-(function($) {
-
-    'use strict';
-
-    $.fn.activateBox = function() {
-        $.GUI.boxWidget.activate(this);
-    };
-
-    $.fn.toggleBox = function() {
-        var button = $($.GUI.boxWidget.selectors.collapse, this);
-        $.GUI.boxWidget.collapse(button);
-    };
-
-    $.fn.removeBox = function() {
-        var button = $($.GUI.boxWidget.selectors.remove, this);
-        $.GUI.boxWidget.remove(button);
-    };
-
-})(jQuery);
-
-/*
- * TODO LIST CUSTOM PLUGIN
- * -----------------------
- * This plugin depends on iCheck plugin for checkbox and radio inputs
- *
- * @type plugin
- * @usage $("#todo-widget").todolist( options );
- */
-(function($) {
-
-    'use strict';
-
-    $.fn.todolist = function(options) {
-        // Render options
-        var settings = $.extend({
-            //When the user checks the input
-            onCheck: function(ele) {
-                return ele;
-            },
-            //When the user unchecks the input
-            onUncheck: function(ele) {
-                return ele;
-            }
-        }, options);
-
-        return this.each(function() {
-
-            if (typeof $.fn.iCheck != 'undefined') {
-                $('input', this).on('ifChecked', function() {
-                    var ele = $(this).parents("li").first();
-                    ele.toggleClass("done");
-                    settings.onCheck.call(ele);
-                });
-
-                $('input', this).on('ifUnchecked', function() {
-                    var ele = $(this).parents("li").first();
-                    ele.toggleClass("done");
-                    settings.onUncheck.call(ele);
-                });
-            } else {
-                $('input', this).on('change', function() {
-                    var ele = $(this).parents("li").first();
-                    ele.toggleClass("done");
-                    if ($('input', ele).is(":checked")) {
-                        settings.onCheck.call(ele);
-                    } else {
-                        settings.onUncheck.call(ele);
-                    }
-                });
-            }
-        });
-    };
-}(jQuery));
 
 // slider of x-axis
 $("#xAxis").slider();
